62343aec054c78dbda3d8af914e3a0f027eeecf1
isawzz <iwgajam@gmail.com>
2020-03-24 20:13:42 -0700

    save

--- wCOMMON/js/package.js
+++ wCOMMON/js/package.js
@@ -1,73 +1,170 @@
 
-function fromPath(path){
-	//pat could be 0.buildings.farms (first is oid!)
-	//return: value of that path (can be list,object, or literal)
-	let parts = path.split('.');
-	let oid = path[0];
-	let o = serverData.table[oid];
-	if (!o) o=serverData.players[oid];
-	let props = parts.slice(1);
-	let res = lookup(o,props);
-	console.log(props,res);
-	return res;
-}
-function matchPath(path){
-	//eg. path: Tick.buildings.farms (first is obj_type!)
-	//return: list of matching oids (can be [] or [oid1,oid2,...]), ie., oids that provide this path!
+function findPlacements(dPlacements, data) {
+	let placements = [];
+	//console.log(SPEC.placement);
 
-}
-function toPath(oid,keys){}
-function prepPackaging(pool,spec){
-	let res = [];
-	let placement = spec.placement;
-	console.log(placement)
-	// for (const (x,y) of placement) {
-	// 	console.log(getTypeOf(x),x); //path,loc)
-	// }
+	for (const placement of SPEC.placement) {
+		let path = Object.keys(placement)[0];
+		let loc = placement[path];
+		let isSelfLoc = startsWith(loc, 'self.');
+		let locProperty = isSelfLoc ? stringAfter(loc, '.') : null; //only allow simple val here! (not nested)
+		//console.log(path, loc, 'isSelfLoc:' + isSelfLoc);
 
-}
-function prepPackaging(pool, spec) {
-	//im ersten vorlauf muss ich table durchgehen und alle objects die in placements vorkommen in 
-	//order of placements in eine liste arrangen
+		let matchingOids = [];
 
-	//opponent.buildings
-	//   rsgType: labelFlex
-	//hallo
-	
+		//path is obj_type.prop1.prop2...propN or .prop1.prop2...propN (regardless of obj_type)
+		//eg. GamePlayer.items
+		//find all objects in serverData that match this path
+		let isTyped = path[0] != '.';
+		let parts = path.split('.');
+		let otype = isTyped ? parts[0] : null;
+		let props = isTyped ? parts.slice(1) : parts;
+		//TODO: add indexing: farms.0 zum beispiel!
+		//or
+		//interpret prop that is a number as index when val is a list
 
-	
+		//console.log('isTyped', isTyped, otype, 'props', props)
 
+		for (const kPool of ['table', 'players']) {
+			let pool = serverData[kPool];
+			let isTable = kPool == 'table';
+			for (const oid in pool) {
 
-	let res = [];
-	let placement = spec.placement;
-	for (const place of placement) {
-		//console.log(place);
-		let otype = Object.keys(place)[0];  //eg. Tick
-		let loc = place[otype];
+				let o = pool[oid];
+				if (isTyped && o.obj_type != otype) continue;
 
-		let olist = [];
-		//look for corresponding object(s) for this placement!
-		for (const oid in pool) {
-			let o = pool[oid];
-			//o.oid = oid;
-			let gsmType = o.obj_type; //only consider type placements! no paths
-			if (gsmType == otype) { olist.push({oid:oid}); }
+				//omap is always just 1 element!!!
+				let omap = lookup(o, props);
+				if (!omap) continue;
+
+				// TODO!!!! eval loc if self loc: kann ich auch bei rep machen!!!
+				//denn es koennte auch ein object sein das noch nicht existiert!
+				//in dem fall braeuchte ich uid von dem object!
+				//let myLoc = isSelfLoc ? findLoc(o, locProperty) : loc;
+
+				let match = { omap: omap, loc: loc, path: path, oid: oid, props: props };
+				matchingOids.push(match);
+
+				//loc is self.prop1.prop2...propN or areaName
+			}
+
 		}
-		let vis = spec.visualize[otype];
-		if (!vis || isEmpty(olist)) continue; //no visualization means NO presentation for now (sonst: default)
-		let rsgType = vis.rsgType;
-		let pack = { olist: olist, pool: pool, loc: loc, rsgType: rsgType, params: vis };
-		res.push(pack);
+		placements.push(matchingOids);
 	}
-	return res;
+	// console.log('placements',placements);
+	return placements;
 }
 
-function package(pool, spec) {	
-	let res = prepPackaging(pool, spec);	
+function flattenMappings(placements) {
+	let flattened = [];
+	for (const plc of placements) {
+		for (const match of plc) {
+			let result = [];
+			flattenMatch(match, result);
+			flattened = flattened.concat(result);
+		}
+	}
+	//console.log(flattened);
+	//for (const c of flattened) { consoutput(c.path, c.olist, c.loc); }
+	return flattened;
 
-	//res.map(pack=>pack.olist.map(o=>o.ui=window[pack.rsgType](o,pack)));
-
-	return res;
 }
+function flattenMatch(m, matches) {
+	// returns list of matches made of this single match 
+	//console.log(m);
+	//example: Board 
+	if (SPEC.visualize && SPEC.visualize[m.path]) { matches.push(m); return; }
+	//example: GamePlayer.items
+	//if it is a set of objects, look what type objects are
+	//if objects are sets also, need to go deeper! otherwise just return item list
+	let els = getElements(m.omap);
+	//console.log(els)
+	if (!isEmpty(els)) {
+		//check if els[0] is a literal if yes, return m
+		if (isLiteral(els[0])) {
+			//console.log('literal:', els[0])
+			m.olist = els; matches.push(m); return;
+		} else if (isList(els[0]) || els[0]._set) {
+			//console.log('muss ich jetzt einen neuen match machen?', els[0], m);
+			let i = 0;
+			for (const el of els) {
+				//TODO: soll ich da einen index dazugeben??? glaub schon!!!
+
+				let mNew = { omap: el, loc: m.loc, myLoc: m.myLoc, path: m.path + '.' + i, oid: m.oid, props: m.props };
+				flattenMatch(mNew, matches);
+				i += 1;
+				// //els[0] is dict or array
+				// //only support sets of similar element types!!!
+				// let els1 = getElements(els[0]);
+				// console.log('recursion deeper:', els1);
+			}
+		} else {
+			//console.log('WEISS NICHT WAS TUN MIT', m.omap);
+		}
+
+	} else if (isEmpty(m.props)) {
+		if (!m.omap.obj_type) {
+			console.log('DAS GIBT ES NICHT!!!!! oid omap does NOT have obj_type', m.omap);
+		} else {
+			//console.log('omap is an object w/o visualize directive', m.omap);
+			matches.push(m); return;
+			//should be using detection and default itemType
+
+		}
+		//omap is an object w/ oid
+	} else {
+		//it is some kind of property that happens to be an object, but not an object w/ oid
+		//example player.resources in catan
+		//console.log('WEISS NICHT', m.omap);
+		let itemType = null;
+		for (const key in m.omap) {
+			let val = m.omap[key];
+			if (isLiteral(val) && itemType && itemType != 'literal'
+				|| isList(val) && itemType && itemType != 'list'
+				|| val._set && itemType && itemType != 'list'
+				|| isDict(val) && itemType && itemType != 'obj') {
+				console.log('omap is a mixed object and unknown: DONT KNOW WHAT TO DO!!!!!');
+				console.log('...ignoring this match', m.path, m.omap);
+				//alternatively, could allow such objects and treat them as uniform key-val lists
+				// olist = olist.filter(x => isLiteral(x.value)); // could still do the simplification step!
+				return;
+			} else if (!itemType) {
+				if (isLiteral(val)) itemType = 'literal';
+				else if (isList(val) || val._set) itemType = 'list';
+				else itemType = 'obj';
+			}
+		}
+		//2 cases: either the object has only literal values (resources: {wood:2, brick:3})
+		if (itemType == 'literal') {
+			//da jedes object eine key-value list ist, nimm default rep fuer key-value lists!
+			let olist = dict2olist(m.omap, 'key');
+			m.olist = olist;
+			matches.push(m);
+			return;
+		} else if (itemType == 'list') {
+			//or the object has all sets/lists as values: {farms:[blablabl]}
+			//effectively throws away the keys! but adds olists!
+			for (const key in m.omap) {
+				m.props.push(key);
+				let mNew = { omap: m.omap[key], loc: m.loc, myLoc: m.myLoc, path: m.path + '.' + key, oid: m.oid, props: m.props };
+				flattenMatch(mNew, matches);
+				// //els[0] is dict or array
+				// //only support sets of similar element types!!!
+				// let els1 = getElements(els[0]);
+				// console.log('recursion deeper:', els1);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
 
 
--- work/atest14.js
+++ work/atest14.js
@@ -1,5 +1,5 @@
 //using keys, assets, divs for opps,table,players, full serverData in use!, preProcessedData, handle playerChange/restart
-//placement list
+//packaging steps: 1. mapping placement, 2. flattening lists, 3. adding types (list,item)
 window.onload = () => _start();
 var divMain, divPlayer, divOpps, colors, iColor, packageList, presentationLists, timit;
 
@@ -15,6 +15,7 @@ async function _start() {
 	gameStep();
 	//interaction(); //to test 2nd step
 }
+function findLoc(o, prop) { return o[prop] ? o[prop].toUpperCase() : null; }
 async function gameStep() {
 
 	if (serverData.waiting_for) { await sendStatus(getUsernameForPlid(serverData.waiting_for[0])); }
@@ -23,92 +24,37 @@ async function gameStep() {
 	timit.showTime('* vor package: *')
 
 	preProcessData();
-	//have: serverData (processed), tupleGroups, boats, SPEC, CODE
+	//have d14, u14 ==> serverData (processed), tupleGroups, boats, SPEC, CODE
 
-	//serverData ist aus d14, uspec aus u14
+	// TODO: here I could insert computing diffed serverData
 
-	//test14 should look at placements and gather all items from serverData.players and
-	//serverData.table that match a placement and return for each placement
-	//list of objects,their concrete path,oids,evaluated area name
-	let placements = SPEC.placement;
+	sData = serverData; //these are the data that I actually want to present!
 
-	//expected result:
-	//>system areas should always be ALL CAPITALS
-	// GamePlayer.items: self.name
-	// path:Player1.items,omap:{_set:[{_obj:3},{_obj:4}],loc: PLAYER1
-	//>omap is 1 object, ?or should I say getElements?
-	//val is list of object
-	// path:Tick,omap:[{_obj:0},{_obj:1},{_obj:3},{_obj:4}],loc: MAIN 
-	//val is list of object w/ value Tick
+	//step1: find placements
+	let placements = findPlacements(SPEC.placement, sData);
+	console.log('placements', placements);
 
+	//step2: flatten lists
+	let flattened = flattenMappings(placements);
+	console.log('flattened',flattened);
 
-	//falsch
+	//jedes object hat jetzt eine olist wenn es ein compound ist und nur omap wenn es simple object ist
 
+	//step 3: give each mapping a listType (if compound) and an itemType
+	//default list and itemTypes are div,div
 
-	console.log(placements);
-	for (const placement of placements) {
-		let path = Object.keys(placement)[0];
-		let loc = placement[path];
-		console.log(path, loc);
-		let matches=[];
+	//bisher wurden noch ueberhauptkeine uis created!!!
+	//for ui:
+	//1. need player and opp areas named w/ pl.name.toUpperCase()
 
-		//path is obj_type.prop1.prop2...propN or .prop1.prop2...propN (regardless of obj_type)
-		//eg. GamePlayer.items
-		//find all objects in serverData that match this path
-		let isTyped = path[0] != '.';
-		let parts = path.split('.');
-		let otype = isTyped ? parts[0] : null;
-		let props = isTyped ? parts.slice(1) : parts;
+	//2. need to create other areas as per layout if any! ==>spare that for now!
 
-		console.log('isTyped', isTyped, otype, 'props', props)
+	//3. geh wieder mit beispielen vor!
 
-		for (const kPool of ['table', 'players']) {
-			let pool = serverData[kPool];
-			let isTable = kPool == 'table';
-			for (const oid in pool) {
-
-				let o = pool[oid];
-				if (isTyped && o.obj_type != otype) continue;
-
-				let omap = lookup(o, props);
-				if (!omap) continue;
-
-				let oidlist = getElements(omap);
-				if (isEmpty(oidList)) {
-					//this is a leaf item: put in placement list
-					matches.push([omap]);
-				}
-				if (isElementList(omap))
-
-					console.log('found match:', oid, omap);
+	//wie 
 
 
 
-
-
-
-				//loc is self.prop1.prop2...propN or areaName
-			}
-
-		}
-	}
-
-
-
-	// //here I could insert computing diffed serverData
-	// sData = serverData; //computeUpdatedServerDataOnly()
-	// if (sData == serverData) sData=sData.table; //simplification for now to make package easier!!!
-
-	// packageList = package(sData, SPEC);
-
-	// console.log('packages',packageList);
-
-	// //console.log(fromPath,fromPath('0.buildings.farms'))
-
-	// showServerData(sData);
-	// //showPackages(packageList);
-
-	// //present(presentationLists);
 	// timit.showTime('* nach package: *')
 }
 
